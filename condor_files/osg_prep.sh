932ddbeecd3eeba50bfeea0f06be497c36b32db1
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2023-08-20 16:05:23 +0900

    update

--- condor_files/osg_prep.sh
+++ condor_files/osg_prep.sh
@@ -8,3 +8,12 @@ dos2unix ${1}/doitall.sh
 # change permissions on wrapper script
 chmod 777 ${1}/wrapper_doitall.sh
 chmod 777 ${1}/doitall.sh
+
+# create Start.tar.gz
+orig_wd=$(pwd)
+while read p; do
+  cd $p
+  tar -czf Start.tar.gz mfcl.cfg *.txt yft.*
+  cd orig_wd
+done <${1}/osg_dir.txt
+unset orig_wd
